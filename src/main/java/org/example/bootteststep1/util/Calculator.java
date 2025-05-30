33cdf519ee0bd6578bb4ad5e9b4d36d63f681cae
dochmai382 <dochmai382@gmail.com>
2025-05-30 10:12:55 +0900

    계산기

---
+++ src/main/java/org/example/bootteststep1/util/Calculator.java
@@ -0,0 +1,27 @@
+package org.example.bootteststep1.util;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+//            throw new ArithmeticException("divide by zero");
+            throw new IllegalArgumentException("divide by zero");
+        }
+        return a / b;
+    }
+
+    public boolean isEven(int a) {
+        return a % 2 == 0;
+    }
+}
